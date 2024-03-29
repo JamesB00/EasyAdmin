f478b65a2117baca7409547c9416395314144203
jamesboynton00 <jamesboynton00@gmail.com>
2024-03-29 16:20:21 -0600

    Began implementation of primary service checking, partial implementation for DNS checking

--- client.py
+++ client.py
@@ -1,23 +1,60 @@
 import socket
 import time
+import os
 
-if __name__ =='__main__':
-    counter = 0
+def determineDNS():
+    status = str(os.system('sudo systemctl status named')).split('\n')
 
+    if(status[0].find('could not be found')):
+        return "Service not found on this host"
+
+    for line in status:
+        temp = line.split(": ")
+        if(temp[0] == "Active"):
+            return temp[1]
+
+#Open configuration file, process it into relevant sysInfo dictionary entry (just port for now)
+def popSysInfo():
     desc = open("client.conf", "r")
     temp = desc.readline().split("=")
     sysInfo = {}
     sysInfo[temp[0]] = temp[1]
+    return sysInfo
+    
+
+if __name__ =='__main__':
+    counter = 0
+
+    sysInfo = popSysInfo()
+
     port = sysInfo['PORT']
     print("Port from conf: " + port)
-
+    
+    #Create socket for receiving requests from main server
     new = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     new.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    new.bind(("", 3030))
+    new.bind(("", int(port)))
     new.listen()
+
+    #Accept the server connection, TODO: authenticate based on IP of server specified in conf
     conn, addr = new.accept()
     while(1):
-        new.send("Info about system: counter = "+str(counter)+"\n")
+        info = conn.recv(1024)
+
+        #If connection is over (null message) break
+        if info == b'':
+            print("Session over")
+            break
+        
+        print("Received: " + str(info))
+        
+        dnsRes = ""
+        #Change this to determine function from request based on hashmap
+        if(info.decode('utf-8').split("|")[1] == "dns"):
+            dnsRes = determineDNS()
+
+        print("dnsRes = " + str(dnsRes))
+        #Send back information gathered on the system
+        conn.send(b"Info about system: DNS = "+bytes(str(dnsRes), 'utf-8')+b"\n")
         counter+=1
-        time.sleep(5)
         
--- start.py
+++ start.py
@@ -1,11 +1,13 @@
 import socket
 import threading
 import string
+import time
 
 servicePorts = {"icmp_echo": None}
 
 controlledHosts = []
 
+#Initialize the clients based on the controlled-hosts.txt file located in current directory
 def initClients():
     desc = open("controlled-hosts.txt", "r")
     retArr = []
@@ -14,8 +16,8 @@ def initClients():
         retArr.append(str(line).split(","))
         line = desc.readline()
     return retArr
-    
 
+#Simple echo for now, later will take on requests and sudden error reporting
 def threadFunc(conn, addr):
     print("Hello from threadFunc", flush=True)
     data = None
@@ -25,29 +27,72 @@ def threadFunc(conn, addr):
         conn.sendall(data+b": Rated 10/10")
     return
 
+def clientOutreach(clients):
+    #Persistent
+    while True:
+        #Go through the clients provided by initialization in main
+        for client in clients:
+            addr = client[1]
+            port = client[2]
+
+            print("Sending outreach to "+addr+" on port "+port, flush=True)
+            
+            #Create socket for communication
+            est = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            try:
+                #Connect to the client at the specified port and address
+                est.connect((addr, int(port)))
+
+                #CHECK prefix denotes client-outreach communication
+                strToSend = "CHECK|"
+
+                #Gather services to be checked (specified in conf), send outreach message
+                for i in range (len(client)):
+                    if i < 3:
+                        continue
+                    strToSend+=str(client[i]).rstrip("\n")+"|"  
+                print("Outreach message: " + strToSend)
+                est.sendall(bytes(strToSend, "utf-8"))
+                
+                #Gather data returned from client, process it, close socket and move on
+                retdata = est.recv(1024)
+                print(retdata.decode("utf-8"))
+                est.close()
+            except:
+                print("Host " + addr + " is unavailable, make sure EasyAdmin software is running on this machine")
+                continue
+
+        #Wait 10 seconds before doing the outreach again
+        time.sleep(10)
+
 
 def server():
     print("Starting server\n")
+
+    #Build Hosts from config file
     clients = initClients()
     print("Hosts managed: " + str(clients))
+
+    #Over-time client outreach thread
+    threading.Thread(target=clientOutreach, args=[clients], name="Client_Outreach_Thread").start()
+
+    #For counting and naming connections
     connCount = 0
+
+    #Main thread socket
     new = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     new.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     new.bind(("", 9015))
     new.listen()
     
+    #Main thread for real time error reporting
     while True: 
+        #Accept the connection, TODO: authentication based on IP addresses
         conn, addr = new.accept()
         connCount+=1
         print("Connection "+str(connCount)+" established", flush=True)
         threading.Thread(target=threadFunc, args=(conn, addr), name=('client_%d', connCount)).start()
-        
-    
-    #start listening on port x 
-    #every once and a while, send a gather info request
-    #process the information returned and update the user about it
-    
-
 
 if __name__ == '__main__':
     server()
